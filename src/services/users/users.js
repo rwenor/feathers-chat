dd8d3540ae081c5bb536a3aaff7229bdff223434
Rolf rPro <rolf@axicon.no>
2023-11-02 16:36:55 +0100

    avatar

--- src/services/users/users.js
+++ src/services/users/users.js
@@ -31,14 +31,15 @@ export const user = (app) => {
   app.service(userPath).hooks({
     around: {
       all: [schemaHooks.resolveExternal(userExternalResolver), schemaHooks.resolveResult(userResolver)],
-      find: [  //authenticate('jwt')
+      find: [  authenticate('jwt')
             ],
-      get: [ //authenticate('jwt')
+      get: [ authenticate('jwt')
             ],
       create: [],
       update: [authenticate('jwt')],
       patch: [authenticate('jwt')],
-      remove: [authenticate('jwt')]
+      remove: [authenticate('jwt')
+              ]
     },
     before: {
       all: [schemaHooks.validateQuery(userQueryValidator), schemaHooks.resolveQuery(userQueryResolver)],
